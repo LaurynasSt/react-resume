43667562a93060781fa32cb88f79357e5b747790
orezdrah <l.stackunas@gmail.com>
2021-01-21 10:59:40 +0200

    Add Main. Move style. Clear Code

---
+++ src/app/components/Main/index.js
@@ -0,0 +1,81 @@
+import ContentBlock from "../ContentBlock";
+import Link from "../Link";
+import List from "../List";
+import Pill from "../Pill";
+import JobExperience from "../JobExperience";
+import Divider from "../Divider";
+
+import translations from "../../translations";
+
+import { Fragment } from "react";
+
+import "./index.css";
+
+function Main({ lang }) {
+    return (
+        <main>
+        <section className="App-links-about">
+          <ContentBlock title={translations[lang].personalLinks.title} className="App-links-ul">
+            {translations.en.personalLinks.links.map(({ link, imgSrc, imgAlt, text }) => {
+              return <Link
+                imgSrc={imgSrc}
+                imgAlt={imgAlt}
+                link={link}
+              >
+                {text}
+              </Link>
+            })}
+          </ContentBlock>
+          <ContentBlock title="ABOUT ME">
+            <p className="App-about-text">
+              {translations[lang].aboutMe}
+            </p>
+          </ContentBlock>
+        </section>
+        <section className="App-skills-section">
+          <ContentBlock title={translations[lang].education.educationTitle} className="App-skills">
+            {translations[lang].education.schools.map(({ schools, years, degree }, index, array) => {
+              return <Fragment>
+                <p className="App-educ-inner-text">
+                  {schools}<br />
+                  {years}<br />
+                  {degree}
+                </p>
+                {index !== array.length - 1 && <Divider isShort />}
+              </Fragment>
+            })}
+          </ContentBlock>
+          <ContentBlock title={translations[lang].personalSkills.title} className="App-skills">
+            {translations[lang].personalSkills.skills.map(({ text, level }) => {
+              return <Pill color={level}>{text}</Pill>
+            })}
+          </ContentBlock>
+          <ContentBlock title={translations[lang].technicalSkills.title} className="App-skills">
+            {translations[lang].technicalSkills.skills.map(({ text, level }) => {
+              return <Pill color={level}>{text}</Pill>
+            })}
+          </ContentBlock>
+        </section>
+        <ContentBlock title={translations[lang].workExperience.title} className="App-experience-container">
+          {translations[lang].workExperience.work.map(({ title, company, years, description, workAchievments }) => {
+            return <JobExperience
+              title={title}
+              company={company}
+              years={years}
+              description={description}
+            >
+              <List
+                list={
+                  workAchievments
+                }
+              />
+            </JobExperience>
+          })}
+        </ContentBlock>
+        <Divider />
+      </main>
+    );
+
+}
+
+export default Main;
--- src/app/index.js
+++ src/app/index.js
@@ -1,14 +1,10 @@
-import './index.css';
 import Header from "./components/Header";
-import Pill from './components/Pill';
-import ContentBlock from './components/ContentBlock';
-import Link from './components/Link';
-import JobExperience from './components/JobExperience';
-import List from './components/List';
+import Main from "./components/Main";
 import Footer from "./components/Footer";
-import Divider from './components/Divider';
-import translations from './translations';
-import { Fragment, useState } from 'react';
+
+import { useState } from 'react';
+
+import './index.css';
 
 function App() {
   const [lang, setLang] = useState("en");
@@ -16,67 +12,7 @@ function App() {
   return (
     <div className="App">
       <Header lang={lang} setLang={setLang}></Header>
-      <main>
-        <section className="App-links-about">
-          <ContentBlock title={translations[lang].personalLinks.title} className="App-links-ul">
-            {translations.en.personalLinks.links.map(({ link, imgSrc, imgAlt, text }) => {
-              return <Link
-                imgSrc={imgSrc}
-                imgAlt={imgAlt}
-                link={link}
-              >
-                {text}
-              </Link>
-            })}
-          </ContentBlock>
-          <ContentBlock title="ABOUT ME">
-            <p className="App-about-text">
-              {translations[lang].aboutMe}
-            </p>
-          </ContentBlock>
-        </section>
-        <section className="App-skills-section">
-          <ContentBlock title={translations[lang].education.educationTitle} className="App-skills">
-            {translations[lang].education.schools.map(({ schools, years, degree }, index, array) => {
-              return <Fragment>
-                <p className="App-educ-inner-text">
-                  {schools}<br />
-                  {years}<br />
-                  {degree}
-                </p>
-                {index !== array.length - 1 && <Divider isShort />}
-              </Fragment>
-            })}
-          </ContentBlock>
-          <ContentBlock title={translations[lang].personalSkills.title} className="App-skills">
-            {translations[lang].personalSkills.skills.map(({ text, level }) => {
-              return <Pill color={level}>{text}</Pill>
-            })}
-          </ContentBlock>
-          <ContentBlock title={translations[lang].technicalSkills.title} className="App-skills">
-            {translations[lang].technicalSkills.skills.map(({ text, level }) => {
-              return <Pill color={level}>{text}</Pill>
-            })}
-          </ContentBlock>
-        </section>
-        <ContentBlock title={translations[lang].workExperience.title} className="App-experience-container">
-          {translations[lang].workExperience.work.map(({ title, company, years, description, workAchievments }) => {
-            return <JobExperience
-              title={title}
-              company={company}
-              years={years}
-              description={description}
-            >
-              <List
-                list={
-                  workAchievments
-                }
-              />
-            </JobExperience>
-          })}
-        </ContentBlock>
-        <Divider />
-      </main>
+      <Main lang={lang}></Main>
       <Footer lang={lang}></Footer>
     </div>
   );
